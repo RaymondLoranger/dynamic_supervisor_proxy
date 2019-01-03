ab8d75a7e689f482792a1f9404d070de094e373b
RaymondLoranger <Raymond.Loranger@bell.net>
2019-01-03 12:22:20 -0500

    0.1.0

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule DynamicSupervisor.Proxy.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      name: "Dynamic Supervisor Proxy",
+      name: "DynamicSupervisor Proxy",
       source_url: source_url(),
       description: description(),
       package: package(),
